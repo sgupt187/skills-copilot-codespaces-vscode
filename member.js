5dd588ec0689dad8fc470046dd50b28c935bbfee
Shalu-star <shalukris7@gmail.com>
2023-10-16 11:52:26 +0530

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
