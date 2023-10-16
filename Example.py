810ebc669a7a018febaaec0b65f5014ae17a3e6a
Shalu-star <shalukris7@gmail.com>
2023-10-16 11:46:05 +0530

    Copilot first commit

--- Example.py
+++
@@ -1,9 +0,0 @@
-def calculateNumbers(var1, var2):
-    print("Addition: ", var1 + var2)
-    print("Subtraction: ", var1 - var2)
-    print("Multiplication: ", var1 * var2)
-    print("Division: ", var1 / var2)
-    print("Modulus: ", var1 % var2)
-    print("Exponent: ", var1 ** var2)
-    print("Floor Division: ", var1 // var2)
-    
